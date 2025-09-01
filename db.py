7f4610d95cdd34dadaabcbb1436ea3ed4b76d754
gydnjsdl <khw33352182@gmail.com>
2025-09-01 17:53:34 +0900

    feat : Mongodb 연결 dbConnector, 테이블 어댑터 DbTable
    dbConnector : 몽고DB와 연결을 직접적으로 하는 클래스
    dbTable : dbConnector의 해당 테이블에 연결해 insert, update, delete를 기능을 수행함

---
+++ db.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+class dbConnector:
+    def __init__(self):
+        self.client = MongoClient("mongodb://localhost:27017")
+        self.db = self.client["MyDb"]
+
+class dbTable:
+    def __init__(self, db, tableName):
+        self.table = db[tableName]
+
+    def insert(self, datadict):
+        self.table.insert_one(datadict)
+
+    def delete(self, filter):
+        self.table.delete_one(filter)
+
+    def update (self, filter, newValue):
+        self .table.updated(filter, newValue)
+        
