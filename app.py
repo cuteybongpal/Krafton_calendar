8ccacefda313627dabe29a45f5c5212d8a4c930e
gydnjsdl <khw33352182@gmail.com>
2025-09-02 11:33:42 +0900

    feat: 로그인 완성

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, redirect, request, session
+from flask import Flask, redirect, request, session, make_response
 from db import dbConnector
 from user import UserRepository
 import jinjaUtil
-import os
+import secrets
 
 dbconnector = None
 userRepo = None
@@ -10,6 +10,11 @@ userRepo = None
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'withoutme'
 
+app.config.update(
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_SAMESITE="Lax"
+)
 
 @app.route('/')
 def hello_world():
@@ -28,13 +33,14 @@ def llllll():
     id = request.form.get('email')
     pw =request.form.get('pw')
     isSuccess = userRepo.isSuccessLogin(id, pw)
-    if isSuccess:
-        print("성공함")
-        session['sessionId'] = id
-        print(session['sessionId'])
-    else:
-        print("실패함")
-    return redirect('/')
+    print(id)
+    if not isSuccess:
+        return "로그인 실패"
+    sessionId = secrets.token_urlsafe(32)
+    session['sessionId'] = sessionId
+    session['userId'] = id
+    print(id)
+    return "로그인 성공"
 
 if __name__ == '__main__':
     dbconnector = dbConnector()
