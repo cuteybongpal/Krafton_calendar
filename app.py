9bc8cc9f35569b5cfc3f2debb1267a42a48cda87
emperor hyowonkim <109669991+cuteybongpal@users.noreply.github.com>
2025-09-03 19:02:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def llllll():
     sessionId = secrets.token_urlsafe(32)
     session['sessionId'] = sessionId
     session['userId'] = id
-    print(session['userId'])
+    print(session.get('userId'))
     return redirect('/')
 
 @app.route('/logout', methods=['GET'])
